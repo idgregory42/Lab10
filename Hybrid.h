65f320dd51142f8256978a1a81d7161c1e8bb8e0
idgregory42 <idgregory42@students.tntech.edu>
2016-11-04 11:30:39 -0500

    done

--- Hybrid.h
+++ Hybrid.h
@@ -44,16 +44,11 @@ Hybrid<T>::~Hybrid()
 //simply comment the first implementation out when working on the second implementation
 //use the getKey method to dequeue/remove
 
-<<<<<<< HEAD
 template < class T >
-=======
-template <class T>
->>>>>>> 7018d218036daf22f0d103114d521836506773a3
 bool Hybrid<T>::isEmpty()
 {
 	return q->isEmpty();
 }
-<<<<<<< HEAD
 
 template < class T >
 void Hybrid<T>::enqueue(T* item)
@@ -69,21 +64,15 @@ T* Hybrid<T>::dequeue()
 {
 	//DoubleNode<T>* doubleNode = q->dequeue();
 	T* item = q->dequeue();
-	item = sldl->remove(item);
+	sldl->remove(item->getKey());
 	return item;
-=======
-template <class T>
-bool Hybrid<T>::enqueue(T* item)
-{
-	q-> 
-	sldl-> 
->>>>>>> 7018d218036daf22f0d103114d521836506773a3
 }
 
 template < class T >
 ListDoublyLinkedIterator<T>* Hybrid<T>::iterator()
 {
-	return sldl->iterator();
+	ListDoublyLinkedIterator<T>* iter = sldl->iterator();
+	return iter;
 }
 
 #endif
